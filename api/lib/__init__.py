2f2c5812760b29169bbe983dbfb782e97e719c40
smk762 <smk762@iinet.net.au>
2024-01-16 04:48:48 +0000

    resolve circular imports error on import script

--- api/lib/__init__.py
+++ api/lib/__init__.py
@@ -14,7 +14,7 @@ from lib.cache import (
     load_statsapi_summary,
 )
 from lib.orderbook import Orderbook
-from db.sqldb import SqlQuery
+from db.sqldb import SqlQuery, populate_pgsqldb
 
 COINS = Coins()
 PRICED_COINS = list(set([i.coin.replace("-segwit", "") for i in COINS.with_price]))
