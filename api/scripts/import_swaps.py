52e8ab7a8b2d80e800d96fb81f1c256f6b395939
smk762 <smk762@iinet.net.au>
2024-02-14 09:04:01 +0000

    tweak import script

--- api/scripts/import_swaps.py
+++ api/scripts/import_swaps.py
@@ -2,38 +2,36 @@
 import os
 import sys
 import argparse
-from datetime import date, datetime
+from datetime import datetime
 
 API_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(API_ROOT_PATH)
-from const import RESET_TABLE
 import db.sqldb as db
+from util.logger import logger
 
 
 def parse_date(date_str):
     try:
-        return datetime.strptime(date_str, "%d-%m-%Y").date()
+        return datetime.strptime(date_str, "%Y-%m-%d").date()
     except ValueError:
-        raise argparse.ArgumentTypeError("Invalid date format. Please use D-M-YYYY format.")
+        raise argparse.ArgumentTypeError("Invalid date format. Please use YYYY-M-D format.")
 
 def main():
-    today = datetime.now().date().strftime("%d-%m-%Y")
+    today = datetime.now().date().strftime("%Y-%m-%d")
 
-    desc = 'Import swaps between two dates in the format D-M-YYYY.'
+    desc = 'Import swaps between two dates in the format YYYY-M-D.'
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--start', type=parse_date, metavar='start_date', help='Start date in D-M-YYYY format', default="1-9-2019")
-    parser.add_argument('--end', type=parse_date, metavar='end_date', help='End date in D-M-YYYY format', default=today)
+    parser.add_argument('start', type=parse_date, help='Start date in YYYY-M-D format', default="2019-9-1")
+    parser.add_argument('end', type=parse_date, help='End date in YYYY-M-D format', default=today)
     parser.add_argument('--reset_table', action='store_true', help='Warning: This will dump the table, then recreate it empty.')
 
     args = parser.parse_args()
-
-    print("Start date:", args.start_date)
-    print("End date:", args.end_date)
+    logger.info(f"Importing swaps between {args.start} and {args.end}...")
         
     DB = db.SqlSource()
     if args.reset_table:
         DB.reset_defi_stats_table()
-    DB.import_swaps(start_dt=args.start_date, end_dt=args.end_date)
+    DB.import_swaps(start_dt=args.start, end_dt=args.end)
 
 if __name__ == "__main__":
     main()
