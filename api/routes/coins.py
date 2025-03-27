57b7dc7526e4f0e35c2966fab1b64703e3b54dc0
smk762 <smk762@iinet.net.au>
2025-03-27 06:19:35 +0100

    adds get_swaps_for_coin method

--- api/routes/coins.py
+++ api/routes/coins.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from typing import List, Dict
 from util.cron import cron
 from util.logger import logger
 from models.generic import ErrorMessage, ApiIds
+import db.sqldb as db
 from util.files import Files
 import util.memcache as memcache
 from util.transform import derive
 
 router = APIRouter()
 files = Files()
-
+query = db.SqlQuery()
 
 @router.get(
     "/api_ids/gecko",
@@ -88,3 +90,45 @@ def top_coins():
         "top_swaps": derive.top_coin_by_swap_counts(vols),
         "top_volumes": derive.top_coin_by_volume(vols)
     }
+
+
+@router.get(
+    "/get_swaps_for_coin/{coin}",
+    description="Swaps for an coin matching filter.",
+    responses={406: {"model": ErrorMessage}},
+    response_model=List[db.DefiSwap],
+    status_code=200,
+)
+def get_swaps_for_coin(
+    coin_str: str,
+    start_time: int = 0,
+    end_time: int = 0,
+    pubkey: str | None = None,
+    gui: str | None = None,
+    version: str | None = None,
+    success_only: bool = True,
+    failed_only: bool = False,
+    all_variants: bool = False,
+):
+    try:
+        if start_time == 0:
+            start_time = int(cron.now_utc()) - 86400
+        if end_time == 0:
+            end_time = int(cron.now_utc())
+
+        resp = query.get_swaps_for_coin(
+            start_time=start_time,
+            end_time=end_time,
+            coin=coin_str,
+            gui=gui,
+            version=version,
+            failed_only=failed_only,
+            success_only=success_only,
+            pubkey=pubkey,
+            all_variants=all_variants,
+        )
+        return resp
+    except Exception as e:  # pragma: no cover
+        err = {"error": f"{e}"}
+        logger.warning(err)
+        return JSONResponse(status_code=400, content=err)
